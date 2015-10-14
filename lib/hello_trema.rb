6c2d48ee676ae682bca77a89be3ad587b12c9386
Yo Nishiyama <yo.applesya@gmail.com>
2015-10-14 14:24:24 +0900

    課題提出

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,15 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(_args)
-    logger.info 'Trema started.'
+    cls_name = self.class.name
+    logger.info "Hi! from #{cls_name}"
   end
 
-  def switch_ready(datapath_id)
-    logger.info "Hello #{datapath_id.to_hex}!"
+  def switch_ready(_datapath_id)
+    logger.info 'Hello #{datapath_id.to_hex}!'
+  end
+
+  def switch_disconnected(datapath_id)
+    logger.info "Bye #{datapath_id.to_hex}!"
   end
 end
